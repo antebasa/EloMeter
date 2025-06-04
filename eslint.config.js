85179bc8e4962e258f0ec4be1ac47a3610009721
antebasic <antebasa@gmail.com>
2025-06-04 19:40:51 +0200

    live match

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,11 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
     },
   },
 )
